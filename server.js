fab157394e8005a95708e068cfc0771e10aa3168
karoianroman <romashka99k@gmail.com>
2026-03-10 10:47:43 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./app');
+
+test('GET / should return 200', async () => {
+  await request(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /text/);
+});
