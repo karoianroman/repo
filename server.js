7c4e1e92903eaaefff55126e1dade3c27d09f38b
karoianroman <romashka99k@gmail.com>
2026-03-26 16:40:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const app = require('./app');
-
-// 1. Беремо порт з хмари, якщо його немає — ставимо 3000 для локалу
+c// Ідеальний варіант для Cloud Run та локальної розробки
 const port = process.env.PORT || 3000;
 
-// 2. Додаємо '0.0.0.0', щоб приймати запити ззовні контейнера
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Сервер працює на порту ${port}`);
 });
